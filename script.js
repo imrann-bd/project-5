71ba5f9614f17c4a23a90575e5ba5a089c6bea37
imrann-bd <imrann.info@gmail.com>
2025-08-29 04:31:27 +0600

    some functionality added

---
+++ script.js
@@ -0,0 +1,51 @@
+
+// heart - icon
+// navbar - heart - icon
+// let count = 0;
+// document.getElementById('heart-icon').addEventListener('click', function () {
+//     const navbarDisplay = document.getElementById('navbar-heart-icon')
+
+
+// })
+
+let count = 0;
+const navbarHeares = document.getElementById('navbar-heart-icon');
+
+document.getElementById('heart-icon1').addEventListener('click', function () {
+    count++;
+    navbarHeares.innerText = count;
+});
+
+document.getElementById('heart-icon2').addEventListener('click', function () {
+    count++;
+    navbarHeares.innerText = count;
+});
+document.getElementById('heart-icon3').addEventListener('click', function () {
+    count++;
+    navbarHeares.innerText = count;
+});
+
+document.getElementById('heart-icon4').addEventListener('click', function () {
+    count++;
+    navbarHeares.innerText = count;
+}); document.getElementById('heart-icon5').addEventListener('click', function () {
+    count++;
+    navbarHeares.innerText = count;
+});
+
+document.getElementById('heart-icon6').addEventListener('click', function () {
+    count++;
+    navbarHeares.innerText = count;
+}); document.getElementById('heart-icon7').addEventListener('click', function () {
+    count++;
+    navbarHeares.innerText = count;
+});
+
+document.getElementById('heart-icon8').addEventListener('click', function () {
+    count++;
+    navbarHeares.innerText = count;
+});
+document.getElementById('heart-icon9').addEventListener('click', function () {
+    count++;
+    navbarHeares.innerText = count;
+});
