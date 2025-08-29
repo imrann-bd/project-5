f86141b27fe6dda92754d8c55105daf27cc344eb
imrann-bd <imrann.info@gmail.com>
2025-08-29 12:40:02 +0600

    navar some function added

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-
+// part 1
 let count = 0;
 const navbarHeares = document.getElementById('navbar-heart-icon');
 
@@ -48,35 +48,120 @@ document.getElementById('heart-icon9').addEventListener('click', function () {
 let coins = 100;
 const coinDisplay = document.getElementById('coins');
 const history = document.getElementById('history');
-const callButtons = document.querySelectorAll('#btn-call'); 
+const callButtons = document.querySelectorAll('#btn-call');
 
 callButtons.forEach(button => {
-  button.addEventListener('click', function() {
-    if (coins < 20) {
-      alert("Not enough coins! at lest 20 coins to call");
-      return;
-    }
+    button.addEventListener('click', function () {
+        if (coins < 20) {
+            alert("Not enough coins! at lest 20 coins to call");
+            return;
+        }
 
-    coins -= 20;
-    coinDisplay.innerText = coins;
-    
-     const now = new Date();
-    const timeString = now.toLocaleTimeString();
+        coins -= 20;
+        coinDisplay.innerText = coins;
 
-    const service = this.getAttribute('data-service');
-    const number = this.getAttribute('data-number');
+        const now = new Date();
+        const timeString = now.toLocaleTimeString();
 
-    alert("Calling " + service + " : " + number);
+        const service = this.getAttribute('data-service');
+        const number = this.getAttribute('data-number');
 
-    const li = document.createElement('li');
+        alert("Calling " + service + " : " + number);
+
+        const li = document.createElement('li');
         li.className =
-      "p-5 rounded-xl shadow bg-gray-200 flex flex-end  items-center m-3";
-    li.innerText = service + " - " + number  +  " (time: " +  timeString+')';
-    history.appendChild(li);
-  });
+            "p-5 rounded-xl shadow bg-gray-200 flex flex-end  items-center m-3";
+        li.innerText = service + " - " + number + " (time: " + timeString + ')';
+        history.appendChild(li);
+    });
 });
 
-document.getElementById('btn-clear').addEventListener('click', function() {
-  history.innerHTML = "";
-  alert("History cleared!");
+document.getElementById('btn-clear').addEventListener('click', function () {
+    history.innerHTML = "";
+    alert("History cleared!");
 });
+
+// part 3
+let sumCopy = 0;
+const navbarCopies = document.getElementById('navbar-copies')
+const numberNine = document.getElementById('nineNumber');
+
+document.getElementById('btn-copy1').addEventListener('click', function () {
+    alert('copy number');
+    sumCopy++;
+    navbarCopies.innerText = sumCopy;
+})
+document.getElementById('btn-copy2').addEventListener('click', function () {
+    alert('copy number');
+    sumCopy++;
+    navbarCopies.innerText = sumCopy;
+})
+document.getElementById('btn-copy3').addEventListener('click', function () {
+    alert('copy number');
+    sumCopy++;
+    navbarCopies.innerText = sumCopy;
+})
+document.getElementById('btn-copy4').addEventListener('click', function () {
+    alert('copy number');
+    sumCopy++;
+    navbarCopies.innerText = sumCopy;
+})
+document.getElementById('btn-copy5').addEventListener('click', function () {
+    alert('copy number');
+    sumCopy++;
+    navbarCopies.innerText = sumCopy;
+})
+document.getElementById('btn-copy6').addEventListener('click', function () {
+    alert('copy number');
+    sumCopy++;
+    navbarCopies.innerText = sumCopy;
+})
+document.getElementById('btn-copy7').addEventListener('click', function () 
+{
+    alert('copy number'); 
+    sumCopy++;
+    navbarCopies.innerText = sumCopy;
+})
+document.getElementById('btn-copy8').addEventListener('click', function () {
+    alert('copy number');
+    sumCopy++;
+    navbarCopies.innerText = sumCopy;
+})
+document.getElementById('btn-copy9').addEventListener('click', function () {
+    alert('copy number');
+    sumCopy++;
+    navbarCopies.innerText = sumCopy;
+})
+// document.getElementById('heart-icon2').addEventListener('click', function () {
+//     count++;
+//     navbarHeares.innerText = count;
+// });
+// document.getElementById('heart-icon3').addEventListener('click', function () {
+//     count++;
+//     navbarHeares.innerText = count;
+// });
+
+// document.getElementById('heart-icon4').addEventListener('click', function () {
+//     count++;
+//     navbarHeares.innerText = count;
+// }); document.getElementById('heart-icon5').addEventListener('click', function () {
+//     count++;
+//     navbarHeares.innerText = count;
+// });
+
+// document.getElementById('heart-icon6').addEventListener('click', function () {
+//     count++;
+//     navbarHeares.innerText = count;
+// }); document.getElementById('heart-icon7').addEventListener('click', function () {
+//     count++;
+//     navbarHeares.innerText = count;
+// });
+
+// document.getElementById('heart-icon8').addEventListener('click', function () {
+//     count++;
+//     navbarHeares.innerText = count;
+// });
+// document.getElementById('heart-icon9').addEventListener('click', function () {
+//     count++;
+//     navbarHeares.innerText = count;
+// });
