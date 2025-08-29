f4f3ca8ea46690336fb40df42ee2a478d82ca5d7
imrann-bd <imrann.info@gmail.com>
2025-08-29 09:47:24 +0600

    some call btn function added.

--- script.js
+++ script.js
@@ -1,13 +1,4 @@
 
-// heart - icon
-// navbar - heart - icon
-// let count = 0;
-// document.getElementById('heart-icon').addEventListener('click', function () {
-//     const navbarDisplay = document.getElementById('navbar-heart-icon')
-
-
-// })
-
 let count = 0;
 const navbarHeares = document.getElementById('navbar-heart-icon');
 
@@ -48,4 +39,39 @@ document.getElementById('heart-icon8').addEventListener('click', function () {
 document.getElementById('heart-icon9').addEventListener('click', function () {
     count++;
     navbarHeares.innerText = count;
+});
+
+
+// part2
+
+
+let coins = 100;
+const coinDisplay = document.getElementById('coins');
+const history = document.getElementById('history');
+const callButtons = document.querySelectorAll('#btn-call'); 
+
+callButtons.forEach(button => {
+  button.addEventListener('click', function() {
+    if (coins < 20) {
+      alert("Not enough coins! at lest 20 coins to call");
+      return;
+    }
+
+    coins -= 20;
+    coinDisplay.innerText = coins;
+
+    const service = this.getAttribute('data-service');
+    const number = this.getAttribute('data-number');
+
+    alert("Calling " + service + " : " + number);
+
+    const li = document.createElement('li');
+    li.innerText = service + " - " + number;
+    history.appendChild(li);
+  });
+});
+
+document.getElementById('btn-clear').addEventListener('click', function() {
+  history.innerHTML = "";
+  alert("History cleared!");
 });
